ab9c8be10451137f8f242f6b2cf3b2e547b1ed8b
murnat98 <murnat98@yandex.ru>
2019-11-16 17:51:53 +0300

    simple template created with yandex maps api

--- core/views.py
+++ core/views.py
@@ -1,5 +1,12 @@
 from django.views.generic import TemplateView
 
+from zoohackaton import settings
+
 
 class MapsView(TemplateView):
-    pass
+    template_name = 'core/maps.html'
+
+    def get_context_data(self, **kwargs):
+        if 'maps_api_key' not in kwargs:
+            kwargs['maps_api_key'] = settings.config['MAPS-API']['api_key']
+        return super().get_context_data(**kwargs)
